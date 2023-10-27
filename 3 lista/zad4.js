2a01f9af8ba47c5b5aa92844d1c75765a3b2880f
Mateusz Budzyński <budzynekm09@gmail.com>
2023-10-27 12:48:56 +0200

    exercise 4 done

---
+++ 3 lista/zad4.js
@@ -0,0 +1,10 @@
+function sum(args)
+{
+    var sum = 0
+    for (let i = 0; i< arguments.length;i++)
+    {
+        sum += arguments[i]
+    }
+    return sum
+}
+console.log(sum(1,2,3,4,5,6,7,8,9,10))
