d3c56df4da05016794a832ec423710899e841f2a
Mateusz Budzyński <budzynekm09@gmail.com>
2023-11-12 08:29:48 +0100

    small changes in zad2wlasciwe.js

--- 5 lista/zad2wlasciwe.js
+++ 5 lista/zad2wlasciwe.js
@@ -1,13 +1,9 @@
 const stdin = process.stdin;
 
-
-function petla()
-{
 console.log("podaj swoje umie urzytkowniku")
 stdin.on("data", function(d) {
     console.log("Witaj " + d.toString().trim() );
     process.exit();
 
-})};
+});
 
-petla()
