c8e463b7d61bdcb306787d50dce3b962f40d28e3
Mateusz Budzyński <budzynekm09@gmail.com>
2023-11-12 08:16:35 +0100

    space added

--- 5 lista/zad2wlasciwe.js
+++ 5 lista/zad2wlasciwe.js
@@ -1,8 +1,13 @@
 const stdin = process.stdin;
 
+
+function petla()
+{
 console.log("podaj swoje umie urzytkowniku")
 stdin.on("data", function(d) {
-    console.log("Witaj" + d.toString().trim() );
+    console.log("Witaj " + d.toString().trim() );
     process.exit();
-});
 
+})};
+
+petla()
