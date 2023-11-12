30f77df22b3bc1722caed58de284e32fe8de10b2
Mateusz Budzyński <budzynekm09@gmail.com>
2023-11-12 08:29:20 +0100

    3 task finished

---
+++ 5 lista/zad3.js
@@ -0,0 +1,33 @@
+const stdin = process.stdin;
+const random_number = Math.floor(Math.random() * 100) + 1;
+
+function petla()
+{
+console.log("select number from 1 to 100")
+stdin.on("data", function(d) {
+
+    if (d == random_number)
+    {
+        console.log("gratulation, you won!!!")
+        process.exit();
+    }
+    else if (d < random_number)
+    {
+        console.log("my number is grater than yours")
+    }
+    else 
+    {
+        console.log("my number is smaller than yours")
+    }
+
+})};
+
+// function gra()
+// {
+
+
+
+// }
+
+
+petla()
